9e6e0df9b06ac19036f58b3315ff1fb1bd97c38b
Jonathan Adami <pitilezard@gmail.com>
2021-07-21 08:37:42 +1000

    Ok didn't realise that typescript needs just the types and won't load index.ts... I hope I'm not making circular references there, let's see!

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,8 +3,16 @@ export type Point = {
     y: number;
 };
 
-export { MiniGraphProps, MiniGraphComponent } from "./MiniGraph";
-export { MiniGraphLinesProps, MiniGraphLinesComponent } from "./MiniGraphLines";
-export { MiniGraphAverageProps, MiniGraphAverageComponent } from "./MiniGraphAverage";
-export { MiniGraphVerticalBarsProps, MiniGraphVerticalBarsComponent } from "./MiniGraphVerticalBars";
-export { MiniGraphNormalBandProps, MiniGraphNormalBandComponent } from "./MiniGraphNormalBand";
+export { default as MiniGraph, MiniGraphProps, MiniGraphComponent } from "./MiniGraph";
+export { default as MiniGraphLines, MiniGraphLinesProps, MiniGraphLinesComponent } from "./MiniGraphLines";
+export { default as MiniGraphAverage, MiniGraphAverageProps, MiniGraphAverageComponent } from "./MiniGraphAverage";
+export {
+    default as MiniGraphVerticalBars,
+    MiniGraphVerticalBarsProps,
+    MiniGraphVerticalBarsComponent,
+} from "./MiniGraphVerticalBars";
+export {
+    default as MiniGraphNormalBand,
+    MiniGraphNormalBandProps,
+    MiniGraphNormalBandComponent,
+} from "./MiniGraphNormalBand";
