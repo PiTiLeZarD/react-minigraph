14ecc18dbc6477c03c67952e55a2a542d32c9aa8
Jonathan Adami <pitilezard@gmail.com>
2022-05-24 18:18:22 +1000

    fix the missing elements of the doc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-export { default as MiniGraph } from "./MiniGraph";
-export { default as MiniGraphLines } from "./MiniGraphLines";
-export { default as MiniGraphAverage } from "./MiniGraphAverage";
-export { default as MiniGraphVerticalBars } from "./MiniGraphVerticalBars";
-export { default as MiniGraphNormalBand } from "./MiniGraphNormalBand";
-export { default as MiniGraphGrid } from "./MiniGraphGrid";
+export { default as MiniGraph, MiniGraphProps, MiniGraphComponent } from "./MiniGraph";
+export { default as MiniGraphLines, MiniGraphLinesProps, MiniGraphLinesComponent } from "./MiniGraphLines";
+export { default as MiniGraphAverage, MiniGraphAverageProps, MiniGraphAverageComponent } from "./MiniGraphAverage";
+export {
+    default as MiniGraphVerticalBars,
+    MiniGraphVerticalBarsProps,
+    MiniGraphVerticalBarsComponent,
+} from "./MiniGraphVerticalBars";
+export {
+    default as MiniGraphNormalBand,
+    MiniGraphNormalBandProps,
+    MiniGraphNormalBandComponent,
+} from "./MiniGraphNormalBand";
+export { default as MiniGraphGrid, MiniGraphGridProps, MiniGraphGridComponent } from "./MiniGraphGrid";
